02dbc8d6590affc91e74735c4fc4f7f4a9143795
jsrglc <rgsyjb@163.com>
2020-04-27 09:06:59 +0800

    correct css selector

--- GithubLogin.py
+++ GithubLogin.py
@@ -1,7 +1,48 @@
 import requests
+from pyquery import PyQuery as pq
 
 class Login():
     def __init__(self):
-        self.header = {
-            
+        self.headers = {
+            'Referer': 'https://github.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'host': 'github.com'
         }
+        self.login_url = 'https://github.com/login'
+        self.post_url = 'https://github.com/session'
+        self.session = requests.Session()
+
+    def token(self):
+        response = self.session.get(url=self.login_url, headers=self.headers)
+        doc = pq(response.text)
+        token = doc('#login > form > input:first').attr('value')
+        return token
+    
+    def login(self, name, password):
+        post_data = {
+            'commit': 'Sign in',
+            'utf8': '✓',
+            'authenticity_token': self.token(),
+            'login': name,
+            'password': password
+        }
+
+        response = self.session.post(url=self.post_url, data=post_data, headers=self.headers)
+        if response.status_code == 200:
+            self.repositories(response.text)
+
+    def repositories(self, html):
+        doc = pq(html)
+
+        name = doc('.header-nav-current-user.css-truncate > a > strong').text().strip()
+        print('name: ', name)
+
+        print('respositories:')
+        items = doc('.news #repos-container > ul > li').items()
+        for item in items:
+            repository = item.text().strip()
+            print(repository)
+
+if __name__ == "__main__":
+    login = Login()
+    login.login(name='jsrglc', password='liuchennuaa2010')

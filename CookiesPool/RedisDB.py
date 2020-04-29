eb2661550caaecb024442595c8795e435bfaa3f9
jsrglc <rgsyjb@163.com>
2020-04-29 15:29:19 +0800

    edit generator

--- CookiesPool/RedisDB.py
+++ CookiesPool/RedisDB.py
@@ -1,2 +1,41 @@
 import random
 import redis
+
+class RedisClient():
+    def __init__(self, type, website):
+        '''Initialize RedisDB'''
+        self.db = redis.StrictRedis()
+        self.type = type
+        self.website = website
+
+    def name(self):
+        '''Get the name of Hash'''
+        return '{}:{}'.format(self.type, self.website)
+    
+    def set(self, username, value):
+        '''Set key value pair'''
+        return self.db.hset(self.name(), username, value)
+    
+    def get(self, username):
+        '''Get key value accoding to key name'''
+        return self.db.hget(self.name(), username)
+    
+    def delete(self, username):
+        '''Delete record accoding to key name'''
+        return self.db.hdel(self.name(), username)
+    
+    def count(self):
+        '''Get number of records '''
+        return self.db.hlen(self.name())
+    
+    def random(self):
+        '''Get value randomly'''
+        return random.choice(self.db.hvals(self.name()))
+
+    def usernames(self):
+        '''Get all account information'''
+        return self.db.hkeys(self.name())
+
+    def all(self):
+        '''Get all key value pairs'''
+        return self.db.hgetall(self.name())
---
+++ CookiesPool/generator.py
@@ -0,0 +1,59 @@
+from RedisDB import RedisClient
+import json
+
+class CookiesGenerator():
+    def __init__(self, website='default'):
+        self.website = website
+        self.cookies_db = RedisClient('cookies', self.website)
+        self.accounts_db = RedisClient('accounts', self.website)
+    
+    def new_cookies(self, username, password):
+        '''Get cookies, subclass needs to be overridden.'''
+        raise NotImplementedError("new_cookies: not implemented!")
+
+#    def process_cookies(self, cookies):
+#        '''Handling cookies to dict style'''
+#        dic = {}
+#        for cookie in cookies:
+#            dic[cookie['name']] = cookie['value']
+#        return dic
+
+    def run(self):
+        '''check the cookies in redis'''
+        accounts_usernames = self.accounts_db.usernames()
+        cookies_usernames = self.cookies_db.usernames()
+        for username in accounts_usernames:
+            if not username in cookies_usernames:
+                # if not exist, then generate
+                password = self.accounts_db.get(username)
+                print(' Generating Cookies', ' username: ', username, ' password: ', password)
+                result = self.new_cookies(username, password)
+                if result.get('status') == 1: # login succeeded
+                     if self.cookies_db.set(username,json.dumps(result.get('content'))):
+                        print('save cookies successfully')
+                else:                         # login failed
+                    print(result.get('content'))
+                    if self.accounts_db.delete(username):
+                        print('account deleted successfully')
+    
+class GithubCookiesGenerator(CookiesGenerator):
+    def __init__(self, website='github'):
+        CookiesGenerator.__init__(self, website)
+
+    def new_cookies(self, username, password):
+        '''Get cookies
+
+        return:
+            dict:{
+                'status': status code, # 1: success, 0: fail
+                'content': cookies
+            }
+        '''
+        return {}
+
+if __name__ == "__main__":
+    redis = RedisClient('accounts', 'github')
+    redis.set('jsrglc', 'liuchennuaa2010')
+
+    generator = GithubCookiesGenerator()
+    generator.run() 

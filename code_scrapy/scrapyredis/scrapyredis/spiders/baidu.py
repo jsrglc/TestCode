b5ab446dcd19b61ed53041317352fad77a9429f6
jsrglc <rgsyjb@163.com>
2020-05-28 16:03:42 +0800

    New Scrapy_Redis Exp.

---
+++ code_scrapy/scrapyredis/scrapyredis/spiders/baidu.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BaiduSpider(scrapy.Spider):
+    name = 'baidu'
+    allowed_domains = ['www.baidu.com']
+    base_urls = 'http://www.baidu.com/s?wd='
+
+    def start_requests(self):
+        for i in range(10):
+            url = self.base_urls + str(i)
+            yield scrapy.Request(url, callback=self.parse)
+
+        for i in range(20):
+            url = self.base_urls + str(i)
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        pass

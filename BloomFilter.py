1ce1fba28b85ca4ad7d1e0ad5bbee4e53c0c6372
jsrglc <rgsyjb@163.com>
2020-05-28 14:53:15 +0800

    refresh bloomfilter_bit

--- BloomFilter.py
+++ BloomFilter.py
@@ -1,7 +1,7 @@
 import redis
 
 BLOOMFILTER_HASH_NUMBER = 6
-BLOOMFILTER_BIT = 5
+BLOOMFILTER_BIT = 10
 
 class HashMap():
     def __init__(self, m, seed):
@@ -21,3 +21,35 @@ class BloomFilter():
         self.maps = [HashMap(self.m, seed) for seed in self.seeds]
         self.server = server
         self.key = key
+
+    def exists(self, value):
+        if not value:
+            return False
+        exist = 1
+        for map in self.maps:
+            offset = map.hash(value)
+            exist = exist & self.server.getbit(self.key, offset)
+        return exist
+    
+    def insert(self, value):
+        for f in self.maps:
+            offset = f.hash(value)
+            self.server.setbit(self.key, offset, 1)
+
+if __name__ == "__main__":
+    conn = redis.StrictRedis()
+    bf = BloomFilter(conn, 'testbf')
+    bf.insert('Hello')
+    bf.insert('World')
+    bf.insert('Python')
+    bf.insert('c++')
+    result = bf.exists('Hello')
+    print(bool(result))
+    result = bf.exists('python')
+    print(bool(result))
+    result = bf.exists('Python')
+    print(bool(result))
+    result = bf.exists('C++')
+    print(bool(result))
+    result = bf.exists('pYTHON')
+    print(bool(result))
